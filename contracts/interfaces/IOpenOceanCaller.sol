5081005b8702cb78b929ea1a3ddbbc89bf737304
LightKool <lq.rockfeller@gmail.com>
2021-08-17 14:45:09 +0800

    initial commit

---
+++ contracts/interfaces/IOpenOceanCaller.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IOpenOceanCaller {
+    struct CallDescription {
+        uint256 target;
+        uint256 gasLimit;
+        uint256 value;
+        bytes data;
+    }
+
+    function makeCall(CallDescription memory desc) external;
+
+    function makeCalls(CallDescription[] memory desc) external payable;
+}
